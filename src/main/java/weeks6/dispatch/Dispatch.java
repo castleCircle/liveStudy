a8a660855c1f82a8837b805510e794e5f2064bfc
yoonemong <yoonemong@naver.com>
2020-12-25 22:11:01 +0900

    no message

--- src/main/java/weeks6/dispatch/Dispatch.java
+++ src/main/java/weeks6/dispatch/Dispatch.java
@@ -4,34 +4,59 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Dispatch {
-
-    static abstract class Service{
-        abstract void run();
+    interface Post {
+        void postOn(SNS sns);
     }
-
-    static class MyService1 extends Service{
+    static class Text implements Post{
         @Override
-        void run() {
-            System.out.println("run1");
+        public void postOn(SNS sns) {
+            sns.post(this);
+        }
+    }
+    static class Picture implements Post{
+        @Override
+        public void postOn(SNS sns) {
+            sns.post(this);
         }
     }
 
-    static class MyService2 extends Service{
-        @Override
-        void run() {
-            System.out.println("run2");
-        }
+    interface SNS{
+        void post(Text post);
+        void post(Picture post);
     }
+    static class Facebook implements SNS{
+
+        @Override
+        public void post(Text post) {
+            System.out.println("text-facebook");
+        }
+
+        @Override
+        public void post(Picture post) {
+            System.out.println("picture-facebook");
+        }
+    };
+    static class Twitter implements SNS{
+
+        @Override
+        public void post(Text post) {
+            System.out.println("text-twitter");
+        }
+
+        @Override
+        public void post(Picture post) {
+            System.out.println("picture-twitter");
+        }
+    };
 
     public static void main(String[] args) {
-        List<Service> svc = Arrays.asList(new MyService1(),new MyService2());
-        svc.forEach(Service::run);
+        List<Post> posts = Arrays.asList(new Text(),new Picture());
+        List<SNS> sns = Arrays.asList(new Facebook(),new Twitter());
+
+        posts.forEach(p->sns.forEach(s->p.postOn(s)));
     }
-
-
-
-
 }
+
 
 
 ////static dispatch란?
@@ -66,7 +91,156 @@ public class Dispatch {
 //}
 //
 //
-//////1. static dispatch란?
+//////1. static dispatch란? 컴파일 시점
 //Method Signature ( name , parameter types ) 같으면 오버라이딩 가능
 //Method Type (return type , method type parameter , method argument types , exception) => 메소드 타입이 같아야 Method Reference를 사용할수 있다.
+//Dynamic Dispatch 를 두번하는게 더블 디스패치
 
+
+//public class Dispatch {
+//
+//    static abstract class Service{
+//        abstract void run();
+//    }
+//
+//    static class MyService1 extends Service{
+//        @Override
+//        void run() {
+//            System.out.println("run1");
+//        }
+//    }
+//
+//    static class MyService2 extends Service{
+//        @Override
+//        void run() {
+//            System.out.println("run2");
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        List<Service> svc = Arrays.asList(new MyService1(),new MyService2());
+//        svc.forEach(Service::run);
+//    }
+//
+//
+//
+//
+//}
+
+
+//public class Dispatch {
+//
+//    interface Post {
+//        void postOn(SNS sns);
+//    }
+//
+//    static class Text implements Post{
+//        @Override
+//        public void postOn(SNS sns) {
+//            if(sns instanceof Facebook){
+//                System.out.println("text - facebook");
+//            }
+//
+//            if(sns instanceof Twitter){
+//                System.out.println("text - twitter");
+//            }
+//        }
+//    }
+//
+//    static class Picture implements Post{
+//        @Override
+//        public void postOn(SNS sns) {
+//            if(sns instanceof Facebook){
+//                System.out.println("Picture - facebook");
+//            }
+//
+//            if(sns instanceof Twitter){
+//                System.out.println("Picture - twitter");
+//            }
+//        }
+//    }
+//
+//    interface SNS{}
+//    static class Facebook implements SNS{
+//
+//    };
+//
+//    static class Twitter implements SNS{
+//
+//    };
+//
+//    static class GooglePlus implements SNS{
+//
+//    };
+//
+//    public static void main(String[] args) {
+//        List<Post> posts = Arrays.asList(new Text(),new Picture());
+//        List<SNS> sns = Arrays.asList(new Facebook(),new Twitter(), new GooglePlus());
+//
+//        posts.forEach(p->sns.forEach(s->p.postOn(s)));
+//    }
+//
+//
+//
+//}
+
+
+//public class Dispatch {
+//
+//    interface Post {
+//        void postOn(SNS sns);
+//    }
+//
+//    static class Text implements Post{
+//
+//        @Override
+//        public void postOn(SNS sns) {
+//            sns.post(this);
+//        }
+//    }
+//
+//    static class Picture implements Post{
+//        @Override
+//        public void postOn(SNS sns) {
+//            sns.post(this);
+//        }
+//    }
+//
+//    interface SNS{
+//        void post(Text post);
+//        void post(Picture post);
+//    }
+//    static class Facebook implements SNS{
+//
+//        @Override
+//        public void post(Text post) {
+//            System.out.println("text-facebook");
+//        }
+//
+//        @Override
+//        public void post(Picture post) {
+//            System.out.println("picture-facebook");
+//        }
+//    };
+//
+//    static class Twitter implements SNS{
+//
+//        @Override
+//        public void post(Text post) {
+//            System.out.println("text-twitter");
+//        }
+//
+//        @Override
+//        public void post(Picture post) {
+//            System.out.println("picture-twitter");
+//        }
+//    };
+//
+//
+//    public static void main(String[] args) {
+//        List<Post> posts = Arrays.asList(new Text(),new Picture());
+//        List<SNS> sns = Arrays.asList(new Facebook(),new Twitter());
+//
+//        posts.forEach(p->sns.forEach(s->p.postOn(s)));
+//    }
+//}
